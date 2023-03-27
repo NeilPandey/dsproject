ec9a601bb97728fddbe65765877fa81bbcbc0aa9
neilpandey <pandey.nilesh1302@gmail.com>
2023-03-27 19:40:34 +0530

    model training, transformation and ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,5 +1,8 @@
 import os
 import sys
+
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
@@ -41,7 +44,6 @@ class DataIngestion:
             return (
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
-
             )
         except Exception as e:
             raise CustomException(e, sys)
@@ -50,4 +52,8 @@ class DataIngestion:
 if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data, test_data)
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
 
