9676b2c821c96622cf42e3b39830b59d4bd3cdab
neilpandey <pandey.nilesh1302@gmail.com>
2023-04-01 16:19:11 +0530

    Adding all the supported py files to run model

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -83,7 +83,9 @@ class ModelTrainer:
                 }
 
             }
-            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test,models=models,param=params)
+            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train,
+                                                 X_test=X_test, y_test=y_test,
+                                                 models=models,param=params)
 
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
@@ -95,7 +97,7 @@ class ModelTrainer:
             ]
             best_model = models[best_model_name]
 
-            if best_model_score < 0.6:
+            if best_model_score < 0.7:
                 raise CustomException("No best model found")
             logging.info(f"Best found model on both training and testing dataset")
 
